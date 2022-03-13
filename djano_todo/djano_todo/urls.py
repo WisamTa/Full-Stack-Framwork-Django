34d8d2c4360b64976bdf4c1b4aba9f85b7545b65
WisamTa <wisamtaqaly@gmail.com>
2022-03-13 14:18:50 +0000

    Editing from todo_views import to from todo.views import say_hello

--- djano_todo/djano_todo/urls.py
+++ djano_todo/djano_todo/urls.py
@@ -1,4 +1,4 @@
-"""django_todo URL Configuration
+"""djano_todo URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from todo.views import say_hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', say_hello, name='hello')
 ]
